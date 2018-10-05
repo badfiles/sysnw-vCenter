f1fe5b05df9b91e2613d000225dae0c5cb71e164
Vladimir Shulev <badfiles@mail.ru>
2018-10-05 10:24:59 +0300

    all done except real connection

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import string
 import time
 import ssl
 import ConfigParser
+import MySQLdb
 
 from pyVmomi import vim, vmodl
 from datetime import datetime
@@ -16,123 +17,183 @@ from pyVim import connect
 from pyVim.connect import Disconnect, SmartConnect, GetSi
 
 def to_bool(value):
-	valid = { 'true': True, 't': True, '1': True, 'yes': True,
-	          'false': False, 'f': False, '0': False, 'no': False }
-	if isinstance(value, bool):
-		return value
-	if not isinstance(value, basestring):
-		raise ValueError('invalid literal for boolean. Not a string.')
-	lower_value = value.lower()
-	if lower_value in valid:
-		return valid[lower_value]
-	else:
-		raise ValueError('invalid literal for boolean: "%s"' % value)
+    valid = { 'true': True, '1': True, 'yes': True,
+              'false': False, '0': False, 'no': False }
+    if isinstance(value, bool):
+        return value
+    if not isinstance(value, basestring):
+        raise ValueError('invalid literal for boolean. Not a string.')
+    lower_value = value.lower()
+    if lower_value in valid:
+        return valid[lower_value]
+    else:
+        raise ValueError('invalid literal for boolean: "%s"' % value)
 
 def GetHumanReadable(size,precision=2):
-	suffixes=['B','KB','MB','GB','TB']
-	suffixIndex = 0
-	while size > 1024 and suffixIndex < 4:
-		suffixIndex += 1
-		size = size/1024.0
-	return "%.*f%s"%(precision, size, suffixes[suffixIndex])
+    suffixes=['B','KB','MB','GB','TB']
+    suffixIndex = 0
+    while size > 1024 and suffixIndex < 4:
+        suffixIndex += 1
+        size = size/1024.0
+    return "%.*f%s"%(precision, size, suffixes[suffixIndex])
 
 def read_config(section):
-	config = ConfigParser.RawConfigParser()
-	config.read('cred.conf')
-	return config.items(section)
+    config = ConfigParser.RawConfigParser()
+    config.read('cred.conf')
+    return config.items(section)
 
 def random_vm(): # vm [name, disksize]
-	vm=[]
-	vm.append(''.join(random.choice(string.ascii_lowercase) for _ in range(5)))
-	vm.append(random.randint( 0, 5000000000) )
-	return vm
-	
+    vm=[]
+    vm.append(''.join(random.choice(string.ascii_lowercase) for _ in range(5)))
+    vm.append(random.randint( 0, 5000000000) )
+    return vm
+    
 def random_snapshot(vm,size): # snapshot [vm name, name, size, timestamp]
-	ss=[]
-	ss.append(vm)
-	ss.append(''.join(random.choice(string.ascii_lowercase) for _ in range(3)))
-	while True:
-		ssize = random.randint( 0, 5000000000)
-		if ssize <= int(size): break;
-	ss.append(ssize)
-	ss.append('152' + ''.join(random.choice(string.digits) for _ in range(7)))
-	return ss
+    ss=[]
+    ss.append(vm)
+    ss.append(''.join(random.choice(string.ascii_lowercase) for _ in range(3)))
+    while True:
+        ssize = random.randint( 0, 5000000000)
+        if ssize <= int(size): break
+    ss.append(ssize)
+    ss.append('152' + ''.join(random.choice(string.digits) for _ in range(7)))
+    return ss
 
 def main():
-	GC = read_config('general')
-	VC = read_config('vCenter')
-	runtime = int(time.time())
-	do_mysql = False
+    GC = read_config('general')
+    VC = read_config('vCenter')
+    do_mysql = False
+    vms=[]
+    sss=[]
 
-	for item in GC:
-		if item[0].lower() == 'snapshots':  snapshots = int(item[1])
-		if item[0].lower() == 'ratio':      ratio =     int(item[1])
-		if item[0].lower() == 'debug':      debug =     to_bool(item[1])
-		if item[0].lower() == 'output' and item[1].lower() == 'mysql':
-			DBC = read_config('mySql')
-			do_mysql = True
+    for item in GC:
+        if item[0].lower() == 'snapshots':   snapshots =   int(item[1])
+        if item[0].lower() == 'ratio':       ratio =       int(item[1])
+        if item[0].lower() == 'debug':       debug =       to_bool(item[1])
+        if item[0].lower() == 'random_data': random_data = to_bool(item[1])
+        if item[0].lower() == 'output' and item[1].lower() == 'mysql':
+            DBC = read_config('mySql')
+            do_mysql = True
 
-	for item in VC:
-		if item[0].lower() == 'vc_address':  vC_address =  item[1]
-		if item[0].lower() == 'vc_port':     vC_port =     int(item[1])
-		if item[0].lower() == 'vc_user':     vC_user =     item[1]
-		if item[0].lower() == 'vc_password': vC_password = item[1]
-		if item[0].lower() == 'vc_ssl':      vC_ssl =      to_bool(item[1])
+    for item in VC:
+        if item[0].lower() == 'vc_address':  vC_address =  item[1]
+        if item[0].lower() == 'vc_port':     vC_port =     int(item[1])
+        if item[0].lower() == 'vc_user':     vC_user =     item[1]
+        if item[0].lower() == 'vc_password': vC_password = item[1]
+        if item[0].lower() == 'vc_ssl':      vC_ssl =      to_bool(item[1])
 
-	if debug: print vC_address, vC_port, vC_ssl, ratio, snapshots, runtime
+    if debug: print vC_address, vC_port, vC_ssl, ratio, snapshots, runtime
+    if not random_data:
+        try:
+            if not vC_ssl:
+                service_instance = connect.SmartConnectNoSSL(host=vC_address,
+                                                             user=vC_user,
+                                                             pwd=vC_password,
+                                                             port=vC_port)
+            else:
+                service_instance = connect.SmartConnect(host=vC_address,
+                                                            user=vC_user,
+                                                            pwd=vC_password,
+                                                            port=vC_port)
 
-	make_vms = 5
-	make_max_snapshots = 10
-	vms=[]
-	for _ in range( make_vms ):
-		vms.append(random_vm())
+            atexit.register(connect.Disconnect, service_instance)
 
-	sss=[]
-	for item in vms:
-		for _ in range(random.randint(0, make_max_snapshots)):
-			sss.append(random_snapshot(item[0],item[1]))
-	if debug:
-		print vms
-		print sss
+            content = service_instance.RetrieveContent()
 
-	passed_vms=[]
-	for vm in vms:
-		machines = 0
-		line=[]
-		exceed = False
-		for snapshot in sss:
-			if snapshot[0] == vm[0]:
-				machines += 1
-				if 100 * int(snapshot[2]) > ratio * int(vm[1]): exceed = True
-		if exceed == True and machines >= snapshots:
-			line.append(vm[0])
-			line.append(vm[1])
-			line.append(machines)
-			line.append(exceed)
-			passed_vms.append(line)
+            container = content.rootFolder
+            viewType = [vim.VirtualMachine]
+            recursive = True
+            containerView = content.viewManager.CreateContainerView(
+                            container, viewType, recursive)
+            children = containerView.view
+            for child in children: pass # parse output to arrays vms and sss if possible here
 
-#	if debug: print passed_vms
+        except vmodl.MethodFault as error:
+            print("Caught vmodl fault : " + error.msg)
+            return -1
+    else:
+        make_vms = 50
+        make_max_snapshots = 5
+        for _ in range( make_vms ):
+            vms.append(random_vm())
+        for item in vms:
+            for _ in range(random.randint(0, make_max_snapshots)):
+                sss.append(random_snapshot(item[0],item[1]))
 
-	if do_mysql:
-		for item in DBC:
-			if item[0].lower() == 'db_address':  db_address =  item[1]
-			if item[0].lower() == 'db_port':     db_port =     int(item[1])
-			if item[0].lower() == 'db_user':     db_user =     item[1]
-			if item[0].lower() == 'db_password': db_password = item[1]
+    if debug:
+        print vms
+        print sss
 
-		if debug: print db_address, db_port
-	else:
-		if passed_vms == []: print '\n No machines have more than ' + str(snapshots) +' snapshots and snapshot/size ratio >' + str(ratio) +'%.'
-		else:
-			for vm in passed_vms:
-				print '\n Machine ' + vm[0] + ' (disks size ' + GetHumanReadable( int(vm[1])) + ') has snapshots'
-				for snapshot in sss:
-					if vm[0] == snapshot[0]:
-						mark = ''
-						if 100 * int(snapshot[2]) > ratio * int(vm[1]): mark = '*'
-						print snapshot[1], '\tCreated: ' + datetime.utcfromtimestamp(int(snapshot[3])).strftime('%m-%d-%Y %H:%M:%S'), '\tSize: ' + GetHumanReadable( int(snapshot[2])) + mark
-	return 0
+    passed_vms=[]
+    for vm in vms:
+        machines = 0
+        line=[]
+        exceed = False
+        for snapshot in sss:
+            if snapshot[0] == vm[0]:
+                machines += 1
+                if 100 * int(snapshot[2]) > ratio * int(vm[1]): exceed = True
+        if exceed == True and machines >= snapshots:
+            line.append(vm[0])
+            line.append(vm[1])
+            passed_vms.append(line)
+
+    if debug: print passed_vms
+
+    if do_mysql:
+        if not passed_vms == []:
+            records = 0
+            for item in DBC:
+                if item[0].lower() == 'db_address':  db_address =  item[1]
+                if item[0].lower() == 'db_port':     db_port =     int(item[1])
+                if item[0].lower() == 'db_user':     db_user =     item[1]
+                if item[0].lower() == 'db_password': db_password = item[1]
+                if item[0].lower() == 'db_base':     db_base =     item[1]
+
+            if debug: print db_address, db_port
+
+            db=MySQLdb.connect(host=db_address,port=db_port,user=db_user,passwd=db_password,db=db_base)
+            dbc=db.cursor() #, created, added)
+            insert_data = []
+            time_to_db = datetime.utcfromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')
+
+            for vm in passed_vms:
+                for snapshot in sss:
+                    if vm[0] == snapshot[0]:
+                        records += 1
+                        line = (snapshot[1],
+                                snapshot[0],
+                                snapshot[2],
+                                datetime.utcfromtimestamp(int(snapshot[3])).strftime('%Y-%m-%d %H:%M:%S'),
+                                time_to_db
+                               )
+                        insert_data.append(line)
+
+            if debug: print insert_data
+
+            dbc.executemany("""INSERT INTO snapshots (sname, vmname, size, created, added)
+                               VALUES (%s, %s, %s, %s, %s)""",
+                            insert_data
+                           )
+            db.commit()
+            db.close()
+            print 'Inserted ' + str(records) +  ' record(s) into the database ' + db_base.upper()
+        else: print 'No records inserted into the database'
+    else:
+        if passed_vms == []: print '\nNo machines have more than ' + str(snapshots) + \
+        ' snapshots and snapshot/size ratio >' + str(ratio) +'%.'
+        else:
+            for vm in passed_vms:
+                print '\nMachine ' + vm[0] + ' (total disk size ' + GetHumanReadable( int(vm[1])) + ') has snapshots'
+                for snapshot in sss:
+                    if vm[0] == snapshot[0]:
+                        mark = ''
+                        if 100 * int(snapshot[2]) > ratio * int(vm[1]): mark = '*'
+                        print snapshot[1], \
+                        '\tCreated: ' + datetime.utcfromtimestamp(int(snapshot[3])).strftime('%d-%m-%Y %H:%M:%S') + ' UTC', \
+                        '\tSize: ' + GetHumanReadable( int(snapshot[2])) + mark
+    return 0
 
 # Start program
 if __name__ == "__main__":
-	main()
+    main()
---
+++ snapshots.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 05, 2018 at 09:02 AM
+-- Server version: 10.2.17-MariaDB-10.2.17+maria~xenial-log
+-- PHP Version: 7.0.32-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `largesnaps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `snapshots`
+--
+
+CREATE TABLE `snapshots` (
+  `id` int(11) NOT NULL,
+  `sname` varchar(255) NOT NULL,
+  `vmname` varchar(255) NOT NULL,
+  `size` bigint(20) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `added` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `snapshots`
+--
+ALTER TABLE `snapshots`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `snapshots`
+--
+ALTER TABLE `snapshots`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
